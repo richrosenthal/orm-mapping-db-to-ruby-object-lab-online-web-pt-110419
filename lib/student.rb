e83f5d700859700902b44002cb580ae1cb9b34d2
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 02:15:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
   
   end 
   
-  def self.first_X_students_in_grade_10(integer)
+  def self.first_X_students_in_grade_10(x)
      sql = <<-SQL
       SELECT *
       FROM students
@@ -68,7 +68,7 @@ class Student
       
     SQL
  
-     DB[:conn].execute(sql).map do |row|
+     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
