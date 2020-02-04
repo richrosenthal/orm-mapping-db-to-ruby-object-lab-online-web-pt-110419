bb51e7a1b250cf2d6d15e3396e9b380fd3074a31
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 01:10:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,11 @@ class Student
   def self.all
      sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
  
-    DB[:conn].execute(sql)
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.find_by_name(name)
