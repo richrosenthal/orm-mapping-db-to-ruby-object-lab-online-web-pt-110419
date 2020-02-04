f7f7d20c458acb5c03355a8281902a377d106383
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 02:03:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,19 @@ class Student
   
   end 
   
+  def self.first_X_students_in_grade_10(x)
+     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+ 
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
