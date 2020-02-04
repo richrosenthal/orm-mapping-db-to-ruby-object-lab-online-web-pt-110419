b9e1126a702cf6a3dd634be38efeb9c627354f83
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 02:00:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,18 @@ class Student
   
   end 
   
+   def self.all_students_below_12th_grade
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+ 
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  
+  end 
   
   def save
     sql = <<-SQL
