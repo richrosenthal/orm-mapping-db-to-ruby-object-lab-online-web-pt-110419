87bcfa0729c8b0d816374cfe1f3111252bca7c48
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 02:16:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,20 @@ class Student
     end
   end
   
+    def self.all_students_in_grade_X(x)
+     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      
+    SQL
+ 
+     DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  
   def self.first_student_in_grade_10
      sql = <<-SQL
       SELECT *
